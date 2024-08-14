eeae09bc34cc3901178075c71489b1613b1a68b5
David Lonni <david_lonni@hotmail.fr>
2024-08-14 12:37:19 +0200

    Dice display
    - Setup profil scene to have dice display and pp display
    - Allow to rotate the dice display

--- LeSuperJeu/Assets/Scripts/UI/SuperProfilMenuComponent.cs
+++ LeSuperJeu/Assets/Scripts/UI/SuperProfilMenuComponent.cs
@@ -1,16 +1,45 @@
 using TMPro;
+using TriInspector;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
+[DeclareTabGroup("Tabs")]
 public class SuperProfilMenuComponent : MonoBehaviour
 {
+    [Group("Tabs"), Tab("UI")]
     public TMP_Text m_NicknameText;
+    [Group("Tabs"), Tab("UI")]
     public TMP_Text m_RankAndVictoryText;
+
+    [Group("Tabs"), Tab("3D")]
+    public Transform m_DiceDisplayStandPivot;
+    [Group("Tabs"), Tab("3D")]
+    public SuperPointerClickHandler m_RenderTextureClickHandler;
+
+    private Vector3 m_DiceDisplayStandPivotInitialRotation;
 
     private void Awake()
     {
         SuperPlayerInfo superPlayerInfo = SuperDataContainer.Instance.m_SuperPlayerInfo;
         m_NicknameText.text = superPlayerInfo.m_Nickname;
         m_RankAndVictoryText.text = $"Rank : {superPlayerInfo.m_GlobalInfo.m_GlobalRank} / Victories : {superPlayerInfo.m_GlobalInfo.m_VictoryCount}";
+
+        m_RenderTextureClickHandler.OnClicked += OnRenderTextureClicked;
+    }
+
+    private void Update()
+    {
+        if(m_RenderTextureClickHandler.IsClicked)
+        {
+            Vector3 newRotation = m_DiceDisplayStandPivot.localEulerAngles;
+            newRotation.y = m_DiceDisplayStandPivotInitialRotation.y + m_RenderTextureClickHandler.DeltaPosition.x;
+            m_DiceDisplayStandPivot.localEulerAngles = newRotation;
+        }
+    }
+
+    private void OnRenderTextureClicked()
+    {
+        m_DiceDisplayStandPivotInitialRotation = m_DiceDisplayStandPivot.localEulerAngles;
     }
 
     public void OnBackButtonClicked()
