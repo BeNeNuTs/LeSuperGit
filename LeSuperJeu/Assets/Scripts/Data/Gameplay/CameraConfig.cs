0278d087b788e2391ad87b02706f1261de1c9193
Mathias Rolland <mathias.rolland@ubisoft.com>
2024-08-20 10:41:33 +0200

    Camera config before merge

---
+++ LeSuperJeu/Assets/Scripts/Data/Gameplay/CameraConfig.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Camera Settings", menuName = "Camera/Global settings")]
+public class CameraConfig : ScriptableObject
+{
+}
