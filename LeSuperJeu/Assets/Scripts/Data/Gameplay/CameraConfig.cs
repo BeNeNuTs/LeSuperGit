f28b4a9eef26c8e09165b2216454be84f5c3f839
Mathias Rolland <mathias.rolland@ubisoft.com>
2024-08-29 16:35:49 +0200

    Super first draft menu camera & remake cam manager

--- LeSuperJeu/Assets/Scripts/Data/Gameplay/CameraConfig.cs
+++ LeSuperJeu/Assets/Scripts/Data/Gameplay/CameraConfig.cs
@@ -4,36 +4,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using System;
-
+[DeclareTabGroup("Camtabs")]
 [CreateAssetMenu(fileName = "Camera Settings", menuName = "SUPER JEU/Camera/Global settings")]
 public class CameraConfig : ScriptableObject
 {
-    [SerializeField]
-    [TableList(Draggable = true, HideAddButton = false, HideRemoveButton = false, AlwaysExpanded = false)]
-    private List<CameraSettingForGameFlowWrapper> m_camerasSettingsForGameFlow;
-    public List<CameraSettingForGameFlowWrapper> CamerasSettingsForGameFlow => m_camerasSettingsForGameFlow;
-    
-    [SerializeField]
-    //[TableList(Draggable = true, HideAddButton = false, HideRemoveButton = false, AlwaysExpanded = false)]
-    private List<CameraSettingForGamePlayFlowWrapper> m_camerasSettingsForGameplayFlow;
-    public List<CameraSettingForGamePlayFlowWrapper> CamerasSettingsForGameplayFlow => m_camerasSettingsForGameplayFlow;
 
+    [SerializeField]
+    private CinemachineVirtualCameraBase m_defaultCamera;
+    public CinemachineVirtualCameraBase DefaultCamera => m_defaultCamera;
+
+    [Group("Camtabs"), Tab("General Gameflow Setting")]
+    [SerializeField]
+    private SerializableDictionary<SuperGameFlowEventManager.EGlobalGameState, CameraSettingForGameFlow> m_camerasSettingsForGameFlow;
+    public SerializableDictionary<SuperGameFlowEventManager.EGlobalGameState, CameraSettingForGameFlow> CamerasSettingsForGameFlow => m_camerasSettingsForGameFlow;
+    
+    
+    [Group("Camtabs"), Tab("Gameplay Flow Setting")]
+    [SerializeField]
+    private SerializableDictionary<SuperGameFlowEventManager.ECurrentGameplayFlowState, CameraSettingForGameFlow> m_camerasSettingsForGameplayFlow;
+    public SerializableDictionary<SuperGameFlowEventManager.ECurrentGameplayFlowState, CameraSettingForGameFlow>CamerasSettingsForGameplayFlow => m_camerasSettingsForGameplayFlow;
+
+    [Group("Camtabs"), Tab("Menu Flow Setting")]
+    [SerializeField]
+    private SerializableDictionary<SuperGameFlowEventManager.EMenuGameState, CameraSettingForGameFlow> m_camerasSettingsForMenuFlow;
+    public SerializableDictionary<SuperGameFlowEventManager.EMenuGameState, CameraSettingForGameFlow> CamerasSettingsForMenuFlow => m_camerasSettingsForMenuFlow;
+
+    [Title("Camera Shake")]
     [SerializeField]
     private CinemachineImpulseDefinition m_impulse;
     public CinemachineImpulseDefinition Impulse => m_impulse;
-
-    [Serializable]
-    public struct CameraSettingForGameFlowWrapper
-    {
-        public SuperGameFlowEventManager.EGlobalGameState GameFlowState;
-        public CameraSettingForGameFlow Setting;
-    }
-    [Serializable]
-    public struct CameraSettingForGamePlayFlowWrapper
-    {
-        public SuperGameFlowEventManager.ECurrentGameplayFlowState GameplayFlowState;
-        public CameraSettingForGameFlow Setting;
-    }
 
     [Serializable]
     public struct CameraSettingForGameFlow
