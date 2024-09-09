a8e95ed7aa185cffd6c4f7dc6930022f38d18415
Mathias Rolland <mathias.rolland@ubisoft.com>
2024-09-09 11:01:50 +0200

    Super anchor for camera

--- LeSuperJeu/Assets/Scripts/SuperGameplay/SuperCameraAnchor.cs
+++ LeSuperJeu/Assets/Scripts/SuperGameplay/SuperCameraAnchor.cs
@@ -7,11 +7,24 @@ public class SuperCameraAnchor : MonoBehaviour
     private SuperCameraIdentifier m_identifier;
     public SuperCameraIdentifier Identifier => m_identifier;
 
-    [SerializeReference]
+    
+    [SerializeField]
+    private bool m_overrideCameraSettings;
+    public bool OverrideCameraSettings => m_overrideCameraSettings;
+
+    [SerializeField]
     private CameraConfig.CameraSetting m_anchorSetup;
-    public CameraConfig.CameraSetting AnchorSetup;
+    public CameraConfig.CameraSetting AnchorSetup => m_anchorSetup;
+
+    private CameraConfig.CameraSetting m_runtimeOverrideSettings;
+    public  CameraConfig.CameraSetting RuntimeOverrideSettings => m_runtimeOverrideSettings;
+
     void Awake()
     {
+        if(OverrideCameraSettings)
+        {
+            GenerateRuntimeSettings();
+        }
         RegisterToSuperDirectionSystem();
     }
 
@@ -31,6 +44,12 @@ public class SuperCameraAnchor : MonoBehaviour
     {
         UnregisterToSuperDirectionSystem();
     }
+
+    private void GenerateRuntimeSettings()
+    {
+        m_runtimeOverrideSettings = SuperDirectionManager.Instance.CameraManager.GenerateCameraSetting(AnchorSetup, Identifier.name.ToString());
+    }
+
     private void RegisterToSuperDirectionSystem()
     {
         SuperDirectionManager.Instance.CameraManager.RegisterAnchor(this);
